54ff45922168a8e33460dd6ff1e0510c89797531
Auxy <auxy@Auxys-MacBook-Pro.local>
2020-05-30 13:08:51 -0400

    Add build folder

---
+++ build/precache-manifest.3e92914af6b1e173c323a71579f94dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd429453e8b46400a6798e0950e09494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d0aa171369711e1b21d",
+    "url": "/static/css/main.1fdea71e.chunk.css"
+  },
+  {
+    "revision": "aa4662cd3ec4d55f398c",
+    "url": "/static/js/2.10f62a64.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.10f62a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0aa171369711e1b21d",
+    "url": "/static/js/main.5a423087.chunk.js"
+  },
+  {
+    "revision": "5268dabde9f94a8fd667",
+    "url": "/static/js/runtime-main.2e2c2112.js"
+  }
+]);
