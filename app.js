4a6dd67adafea37a8ff9c0a89d99914f44c1e3a3
Auxy <auxy@Auxys-MacBook-Pro.local>
2020-05-30 14:43:10 -0400

    remove node_modules

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
